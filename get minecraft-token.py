4f7dd33807b0660cdc31ffdabb78431c07d447d2
ian4u14 <92517191+ian4u@users.noreply.github.com>
2022-01-30 16:29:39 +0100

    Add files via upload
    gets u an thealting token

---
+++ get minecraft-token.py
@@ -0,0 +1,13 @@
+from fileinput import close
+import os
+import pyautogui
+import time
+input('press enter to start')
+os.system('start https://thealtening.com/free/free-minecraft-alts')
+time.sleep(3)
+pyautogui.click(881, 417)
+time.sleep(2)
+pyautogui.click(858, 425)
+time.sleep(13)
+pyautogui.click(165, 908)
+input('press enter to end')
