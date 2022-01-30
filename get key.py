469431e3afdcf08fac5c011a1769efaaef75d2c2
ian4u14 <92517191+ian4u@users.noreply.github.com>
2022-01-30 16:30:52 +0100

    Add files via upload
    gets a krnl(roblox exploid) will need linkvertise premium

---
+++ get key.py
@@ -0,0 +1,18 @@
+import time
+import os
+import pyautogui
+
+input('enter to start')
+os.system('start https://cdn.krnl.ca/getkey.php')
+def go():
+    time.sleep(15)
+    pyautogui.click(1470, 770)
+    time.sleep(3)
+    pyautogui.click(841, 340)
+    time.sleep(3)
+    pyautogui.click(852, 411)
+go()
+go()
+go()
+go()
+go()
